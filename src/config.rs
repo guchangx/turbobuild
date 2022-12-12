cf4ae58b9b35492452de211e6874a42d6df48d94
gucha <gucha@cisco.com>
2022-12-12 18:33:06 +0800

    load configuration form local

---
+++ src/config.rs
@@ -0,0 +1,44 @@
+
+extern crate toml;
+
+#[derive(serde_derive::Deserialize, serde_derive::Serialize, Debug)]
+pub struct ConfigurationInfo {
+    commonder_addr: String,
+    workers_addr: Vec<String>,
+    redis_addr: String,
+}
+
+impl ConfigurationInfo {
+    pub fn init() -> ConfigurationInfo {
+        match fetch_local_config() {
+            Some(value) => {
+                return value;
+            },
+            None => {
+                println!("can't load configuration file, use single tool.");
+                let value = ConfigurationInfo {
+                    commonder_addr: String::from("127.0.0.1"),
+                    workers_addr: vec![String::from("127.0.0.1")],
+                    redis_addr: String::from("")
+                };
+                return value;
+            },
+        } 
+    }    
+}
+
+fn fetch_local_config() -> Option<ConfigurationInfo> {
+    let path = std::env::current_dir().unwrap();
+    let config_file = path.join("config.toml");
+
+    match std::fs::read(config_file) {
+       Ok(contents) => {
+            let config: ConfigurationInfo = toml::from_slice(&contents).unwrap();
+            return Some(config);
+       },
+       Err(error) => {
+        println!("load local configuration file failed {:?}.use default configuration.", error);
+        return None;
+       },
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,12 @@ mod cache;
 mod buildturbo;
 mod dist;
 mod syncfile;
+mod config;
 
 pub fn main() {
     println!("welcome to build turbo tool.");
+    let config = config::ConfigurationInfo::init();
+    println!("config: {:?}", config);
     let working_parameters = buildturbo::WorkingParameters::init();
     network::server::NetworkRequestHandler::start(working_parameters);
     println!("end build turbo tool.");
