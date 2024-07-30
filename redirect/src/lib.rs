30ef943e861b1cab1552074b8c165d772ace1727
guchang <justforsb@outlook.com>
2024-07-30 23:51:17 +0800

    handle detour muilt type return value

--- redirect/src/lib.rs
+++ redirect/src/lib.rs
@@ -13,15 +13,11 @@ static mut ENTRYPOINT: *mut std::ffi::c_void = 0 as _;
 
 #[no_mangle]
 unsafe extern "stdcall" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lpv_reserved: LPVOID) -> BOOL {
-    
-    unsafe {
-        let title = crate::utils::convert::string_2_lpwstr("Debug BreakPoint".to_string());
-        let caption = crate::utils::convert::string_2_lpwstr("Attarch Programe".to_string());
-        winapi::um::winuser::MessageBoxW(0 as winapi::shared::windef::HWND, title.unwrap(), caption.unwrap(), 0);
-    }
+
 
     if crate::detours::DetourIsHelperProcess() == winapi::shared::minwindef::TRUE {
         println!("DllMain is a helper process");
+        //return true;
     }
     else {
         println!("DllMain is a target process");
@@ -33,16 +29,29 @@ unsafe extern "stdcall" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lpv_
             println!("hinstDll: {:?}", hinst_dll);
             println!("lpvReserved: {:?}", lpv_reserved);
 
+            unsafe {
+                println!("message box for process attach");
+                let text = crate::utils::convert::string_2_lpwstr("Debug BreakPoint".to_string());
+                let caption = crate::utils::convert::string_2_lpwstr("Attach Programe".to_string());
+                winapi::um::winuser::MessageBoxW(0 as winapi::shared::windef::HWND, text.unwrap(), caption.unwrap(), 0);
+            }
+
+            //crate::detours::DetourRestoreAfterWithEx(pvData, cbData);
             let ret = crate::detours::DetourRestoreAfterWith();
             if ret == winapi::shared::minwindef::FALSE {
                 let error_code = winapi::um::errhandlingapi::GetLastError();
                 println!("DetourRestoreAfterWith failed, error code: {}.", error_code);
             }
 
-            crate::detours::DetourTransactionBegin();
-    
+            let ret = crate::detours::DetourTransactionBegin();
+            if ret != winapi::shared::winerror::NO_ERROR as i32 {
+                let error_code = winapi::um::errhandlingapi::GetLastError();
+                println!("DetourTransactionBegin failed, erro code: {}.", error_code);
+        
+            }
+
             let ret = crate::detours::DetourUpdateThread(winapi::um::processthreadsapi::GetCurrentThread() as _);
-            if ret == winapi::shared::minwindef::FALSE {
+            if ret != winapi::shared::winerror::NO_ERROR as i32 {
                 let error_code = winapi::um::errhandlingapi::GetLastError();
                 println!("DetourUpdateThread failed, erro code: {}.", error_code);
         
@@ -54,7 +63,11 @@ unsafe extern "stdcall" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lpv_
         
             crate::hook::init_hook();
             
-            crate::detours::DetourTransactionCommit();
+            let ret = crate::detours::DetourTransactionCommit();
+            if ret != winapi::shared::winerror::NO_ERROR as i32 {
+                let error_code = winapi::um::errhandlingapi::GetLastError();
+                println!("DetourTransactionCommit: {}.", error_code);
+            }
 
         },
         winapi::um::winnt::DLL_THREAD_ATTACH => {
@@ -66,6 +79,19 @@ unsafe extern "stdcall" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lpv_
             println!("DLL_PROCESS_DETACH");
             println!("hinstDll: {:?}", hinst_dll);
             println!("lpvReserved: {:?}", lpv_reserved);
+                
+            unsafe {
+                println!("message box for process attach");
+                let text = crate::utils::convert::string_2_lpwstr("Debug BreakPoint".to_string());
+                let caption = crate::utils::convert::string_2_lpwstr("Detach Programe".to_string());
+                winapi::um::winuser::MessageBoxW(0 as winapi::shared::windef::HWND, text.unwrap(), caption.unwrap(), 0);
+            }
+
+            crate::detours::DetourTransactionBegin();
+            crate::detours::DetourUpdateThread(winapi::um::processthreadsapi::GetCurrentThread() as _);
+            //DetourDetach();
+            crate::detours::DetourTransactionCommit();
+
         }
         winapi::um::winnt::DLL_THREAD_DETACH => {
 
