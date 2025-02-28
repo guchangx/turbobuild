bfd067ff3793dc7cb797815fa315b4b6771089c7
GuChang <justforsb@outlook.com>
2025-02-28 10:50:32 +0800

    chore: check lib in redirect build.rs

--- redirect/build.rs
+++ redirect/build.rs
@@ -3,7 +3,15 @@ fn main() {
 
     println!("cargo:rerun-if-changed=build.rs");
     
-    println!("cargo:rerun-if-changed=./3dparty/detours/include/detours.h");
-    println!("cargo:rustc-link-search=native=./cocrew/3dparty/detours/lib.X64");
-    println!("cargo:rustc-link-lib=static=detours");
+    if std::path::Path::new("./../cocrew/3dparty/detours/lib.X64/detours.lib").exists()
+    {
+        println!("cargo:note=redirect cocrew 3dparty detours found.");
+        println!("cargo:rerun-if-changed=./3dparty/detours/include/detours.h");
+        println!("cargo:rustc-link-search=native=./cocrew/3dparty/detours/lib.X64");
+        println!("cargo:rustc-link-lib=static=detours");
+    }
+    else
+    {
+        println!("cargo:warning=redirect cocrew 3dparty detours not found.")
+    }
 }
