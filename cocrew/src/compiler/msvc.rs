c602d4198da655086d6adcb2fd5aa6bfd42ca201
ChangGu <justforsb@outlook.com>
2025-05-21 23:02:03 +0800

    fix: /MP written error

--- cocrew/src/compiler/msvc.rs
+++ cocrew/src/compiler/msvc.rs
@@ -107,10 +107,10 @@ fn request_local_compile_by_preprocessed_source(compiler_input: &CompilerInput) 
     if commands.len() / 2 != 0 {
         combine_commands.push(commands[commands.len() - 1].clone());
     }
-    
+
     //remote compile remove /MP. since multiple cl.exe can not be injected.
     combine_commands.retain(|item| {
-        if item.to_string_lossy() == ("/PM") {
+        if item.to_string_lossy() == ("/MP") {
             return false;
         }
         else {
