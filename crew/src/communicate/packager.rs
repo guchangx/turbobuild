1df7a584f9b0836f0475a5ec2f155a052fb98c0a
ChangGu <justforsb@outlook.com>
2025-06-29 12:15:22 +0800

    feat: sync some dll for cl.exe

--- crew/src/communicate/packager.rs
+++ crew/src/communicate/packager.rs
@@ -35,6 +35,10 @@ impl Packager {
         let mut file = std::fs::File::open(cl.join("Hostx64/x64/cl.exe")).expect("can't find x64 cl.exe");
         let _ = std::io::copy(&mut file, &mut zip);
 
+        let _ = zip.start_file(format!("Hostx64/x64/cl.exe.config"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x64/cl.exe.config")).expect("can't find x64 cl.exe.config");
+        let _ = std::io::copy(&mut file, &mut zip);
+
         let _ = zip.start_file(format!("Hostx64/x64/c1.dll"), options.clone()).unwrap();
         let mut file = std::fs::File::open(cl.join("Hostx64/x64/c1.dll")).expect("can't find x64 c1.dll");
         let _ = std::io::copy(&mut file, &mut zip);
@@ -63,6 +67,24 @@ impl Packager {
         let mut file = std::fs::File::open(cl.join("Hostx64/x64/tbbmalloc.dll")).expect("can't find x64 tbbmalloc.dll");
         let _ = std::io::copy(&mut file, &mut zip);
 
+        let _ = zip.start_file(format!("Hostx64/x64/vcruntime140.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x64/vcruntime140.dll")).expect("can't find x64 vcruntime140.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x64/vcruntime140_1.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x64/vcruntime140_1.dll")).expect("can't find x64 vcruntime140_1.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x64/msvcp140.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x64/msvcp140.dll")).expect("can't find x64 msvcp140.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x64/MSVCP140_ATOMIC_WAIT.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x64/MSVCP140_ATOMIC_WAIT.dll")).expect("can't find x64 MSVCP140_ATOMIC_WAIT.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        //Tracker.exe
+
         let clui = path.clone();
 
         let _ = zip.start_file(format!("Hostx64/x64/1033/clui.dll"), options.clone());
@@ -71,6 +93,10 @@ impl Packager {
 
         let _ = zip.start_file(format!("Hostx64/x86/cl.exe"), options.clone()).unwrap();
         let mut file = std::fs::File::open(cl.join("Hostx64/x86/cl.exe")).expect("can't find x86 cl.exe");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x86/cl.exe.config"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x86/cl.exe.config")).expect("can't find x86 cl.exe.config");
         let _ = std::io::copy(&mut file, &mut zip);
 
         let _ = zip.start_file(format!("Hostx64/x86/c1.dll"), options.clone()).unwrap();
@@ -101,9 +127,27 @@ impl Packager {
         let mut file = std::fs::File::open(cl.join("Hostx64/x86/tbbmalloc.dll")).expect("can't find x86 tbbmalloc.dll");
         let _ = std::io::copy(&mut file, &mut zip);
 
+        let _ = zip.start_file(format!("Hostx64/x86/vcruntime140.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x86/vcruntime140.dll")).expect("can't find x86 vcruntime140.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x86/vcruntime140_1.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x86/vcruntime140_1.dll")).expect("can't find x86 vcruntime140_1.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x86/msvcp140.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x86/msvcp140.dll")).expect("can't find x86 msvcp140.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
+        let _ = zip.start_file(format!("Hostx64/x86/MSVCP140_ATOMIC_WAIT.dll"), options.clone()).unwrap();
+        let mut file = std::fs::File::open(cl.join("Hostx64/x86/MSVCP140_ATOMIC_WAIT.dll")).expect("can't find x86 MSVCP140_ATOMIC_WAIT.dll");
+        let _ = std::io::copy(&mut file, &mut zip);
+
         let _ = zip.start_file(format!("Hostx64/x86/1033/clui.dll"), options.clone());
         let mut file = std::fs::File::open(clui.join("Hostx64/x86/1033/clui.dll")).expect("can't find x86 clui.dll");
         let _ = std::io::copy(&mut file, &mut zip);
+        
+        //Tracker.exe
 
         let content = zip.finish().unwrap();
         let file = content.to_owned().into_inner();
--- crew/src/compiler/msvc.rs
+++ crew/src/compiler/msvc.rs
@@ -311,8 +311,6 @@ impl MSVC {
     
     async fn handle_compile_by_filestream(&self, fileout: &mut FileOut, compiler_input: &CompilerInput, addr: &str, compiler_commands: &Vec<std::ffi::OsString>) 
         -> CompilerOutput {
-        
-        let project_name = &compiler_input.project;
 
         let actions = parse_action_from_commands(&std::ffi::OsString::from("MSBuild"), compiler_commands, &compiler_input.compiler_working_dir);
 
@@ -517,9 +515,9 @@ async fn transmit_precompiled_source_file(compiler_input: &CompilerInput, stream
 
     for file in source_files.to_owned() {
 
-        match precompiled_result.clone() {
+        match &precompiled_result {
             PrecompiledResult::PathWithPCResultName(path) => {
-                intermediate = path;
+                intermediate = path.to_owned();
                 intermediate.set_extension("i");
             },
             PrecompiledResult::PathWithoutPCResultName(path) => {
